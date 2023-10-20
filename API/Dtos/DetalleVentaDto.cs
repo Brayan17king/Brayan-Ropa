5375e0986497ccbbe27bfe8dd4b9469ee86780cc
Brayan17king <xbrayansneyder@gmail.com>
2023-10-20 13:21:21 -0500

    feat: :sparkles: Creation of Dtos

---
+++ API/Dtos/DetalleVentaDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos
+{
+    public class DetalleVentaDto
+    {
+        public int Id { get; set; }
+        public int Cantidad { get; set; }
+        public double ValorUnitario { get; set; }
+        public int MyProperty { get; set; }
+        public int IdTalla { get; set; }
+        public int IdVenta { get; set; }
+        public int IdInventario { get; set; }
+    }
+}
