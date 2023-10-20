36e6fce8e2f609f5dbee46788ad96d0dbd825c90
Brayan17king <xbrayansneyder@gmail.com>
2023-10-20 13:24:30 -0500

    feat: :sparkles: Creation Container of Inyection of Dependency

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,31 @@
+using System.Reflection;
+using API.Extensions;
+using AspNetCoreRateLimit;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.ConfigureRateLimiting();
+builder.Services.ConfigureCors();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+builder.Services.AddApplicationServices();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<RopaContext>(optionsBuilder =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MySqlConex");
+    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
 
 var app = builder.Build();
 
@@ -15,6 +35,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
+app.UseIpRateLimiting();
 
 app.UseHttpsRedirection();
 
